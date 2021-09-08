1b876a29a5c6da18e1b942dc6616627a7c4d58ee
Jordan Felan <felan.jordan94@gmail.com>
2021-09-08 15:09:36 -0500

    added properties and tried to test yahoo finance api

---
+++ src/main/java/com/example/nbastatstracker/NbaStatsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbastatstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStatsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaStatsTrackerApplication.class, args);
+    }
+
+}
